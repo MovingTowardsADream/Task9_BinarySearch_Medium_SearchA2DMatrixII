6b7e03e31fd575b3027d31c7de42c833237499ea
Matvey <zxdfcvas4554@mail.ru>
2023-07-05 19:53:03 +0300

    Solution. All tests accepted

--- Task9_BinarySearch_Medium_SearchA2DMatrixII/Task9_BinarySearch_Medium_SearchA2DMatrixII.cpp
+++ Task9_BinarySearch_Medium_SearchA2DMatrixII/Task9_BinarySearch_Medium_SearchA2DMatrixII.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    static bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return 1;
+            }
+            else if (matrix[i][j] > target) {
+                i--;
+            }
+            else {
+                j++;
+            }
+        }
+        return 0;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<std::vector<int>> matrix = { {1,4,7,11,15} ,{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30} };
+    std::cout << Solution::searchMatrix(matrix, 5);
 }
